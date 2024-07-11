c5dc756e0cdff002fa1331194e97c8d4459f7f4b
shivam-gupta-022 <shivam9410667735@gmail.com>
2024-07-12 01:29:11 +0530

    firstCommit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+// rafce rfce
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
+// componets functions
+function App(){
+
+// statements
+let a1= "sam";
+let num = Math.floor(Math.random()*10);
+let a= 10;
+let b= true;
+let c= false;
+let d = null;
+let e = undefined;
+
+
+  return(
+    <div>
+      <h1>hello from world {a1}</h1>
+      <h1>h {num}</h1>
+      <h1>a:{a}</h1>
+      <h1>b:{JSON.stringify(b)}</h1>
+      <h1>c:{JSON.stringify(c)}</h1>
+      <h1>d:{JSON.stringify(d)}</h1>
+      <h1>e:{JSON.stringify(e)}</h1>
+
+    </div>
+    )
+}
+export default App
+// return for if x
